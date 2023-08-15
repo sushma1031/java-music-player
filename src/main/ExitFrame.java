23830b9e8cd372eb697c4024132fadbe7a5e623a
sushma1031 <sushmajram@gmail.com>
2023-08-15 19:34:22 +0530

    Update ExitFrame.java

--- src/main/ExitFrame.java
+++ src/main/ExitFrame.java
@@ -13,122 +13,100 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class ExitFrame 
-{
+public class ExitFrame {
 	JFrame exitFrame;
 	ImageIcon iconShakey, iconClose, iconOK, iconCancel;
 	Image imageClose, imageOK, imageCancel;
 	JPanel pnlexitHdr;
 	JButton btnCloseExit, btnOK, btnCancel;
 	JLabel lblConfirm;
-	
-	public ExitFrame()
-	{
-		////////////////////////////icon_close starts here ////////////////////////////////////
-		iconClose  = new ImageIcon("src/assets/PNGClose.png");
-		imageClose = iconClose.getImage();
-		imageClose = imageClose.getScaledInstance(39,39, Image.SCALE_SMOOTH);
+
+	public ExitFrame() {
+		// Create close button icon
+		iconClose = new ImageIcon("src/assets/PNGClose.png");
+		imageClose = iconClose.getImage().getScaledInstance(39, 39, Image.SCALE_SMOOTH);
 		iconClose.setImage(imageClose);
-		//////////////////////////// icon_close closed here /////////////////////////////////////
-		
-	}//constructor closed here
-	
-	
-	public void exitframe()
-	{
-		exitFrame=new JFrame("Exit MusicPlayer");
-		exitFrame.setSize(300,150);
+
+	}
+
+	public void exitframe() {
+		exitFrame = new JFrame("Exit Muse.ic");
+		exitFrame.setSize(300, 150);
 		exitFrame.setUndecorated(true);
 		exitFrame.setOpacity(0.9f);
 		exitFrame.setLocationRelativeTo(null);
 		exitFrame.setLayout(null);
 		exitFrame.getContentPane().setBackground(new Color(38, 38, 38));
-		
-		iconShakey  = new ImageIcon("src/assets/shakey.png");
-		Image imageShakey = iconShakey.getImage();
-		iconShakey.setImage(imageShakey);
-		exitFrame.setIconImage(imageShakey);
-    	exitFrame.setVisible(true);
-	    	
-	    pnlexitHdr=new JPanel();
+
+		exitFrame.setVisible(true);
+
+		pnlexitHdr = new JPanel();
 		pnlexitHdr.setBackground(Color.black);
 		pnlexitHdr.setLayout(null);
-		pnlexitHdr.setBounds(0, 0, 300,50);
-	    
-	    btnCloseExit = new JButton();
+		pnlexitHdr.setBounds(0, 0, 300, 50);
+
+		btnCloseExit = new JButton(iconClose);
 		btnCloseExit.setBounds(255, 5, 40, 40);
 		btnCloseExit.setFocusPainted(false);
 		btnCloseExit.setBorderPainted(false);
 		btnCloseExit.setContentAreaFilled(false);
 		btnCloseExit.setToolTipText("Cancel");
 		btnCloseExit.setBackground(Color.black);
-		btnCloseExit.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e) 
-		    {
+		btnCloseExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				exitFrame.dispose();
-		    }          
-
+			}
 		});
-		btnCloseExit.setIcon(iconClose);
-		
-		lblConfirm=new JLabel("Confirm Exit");
-		lblConfirm.setBounds(10,5,235,40);
+
+		lblConfirm = new JLabel("Confirm Exit");
+		lblConfirm.setBounds(15, 5, 235, 40);
 		lblConfirm.setHorizontalAlignment(SwingConstants.CENTER);
 		lblConfirm.setForeground(Color.WHITE);
-		lblConfirm.setFont(new Font("Times New Roman",Font.BOLD,18));
+		lblConfirm.setFont(new Font("Times New Roman", Font.BOLD, 18));
 		pnlexitHdr.add(lblConfirm);
-		
-		
-		iconOK  = new ImageIcon("src/assets/btnOK.png");//Billing_button image
-	    imageOK = iconOK.getImage();
-	    imageOK = imageOK.getScaledInstance(98,38, Image.SCALE_SMOOTH);
-	    iconOK.setImage(imageOK);
-		
-		btnOK=new JButton(iconOK);
-		btnOK.setBounds(30,80,100,40);
+
+		iconOK = new ImageIcon("src/assets/btnOK.png");
+		imageOK = iconOK.getImage().getScaledInstance(98, 38, Image.SCALE_SMOOTH);
+		iconOK.setImage(imageOK);
+
+		btnOK = new JButton(iconOK);
+		btnOK.setBounds(30, 80, 100, 40);
 		btnOK.setBackground(new Color(38, 38, 38));
 		btnOK.setFocusPainted(false);
 		btnOK.setBorderPainted(false);
 		btnOK.setContentAreaFilled(false);
-		btnOK.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e) 
-		    {
+		btnOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
-		    }          
+			}
 
 		});
-		
-		
-		iconCancel  = new ImageIcon("src/assets/btnCancel.png");//Billing_button image
-	    imageCancel = iconCancel.getImage();
-	    imageCancel = imageCancel.getScaledInstance(98,38, Image.SCALE_SMOOTH);
-	    iconCancel.setImage(imageCancel);
-		
-		btnCancel=new JButton(iconCancel);
-		btnCancel.setBounds(170,80,100,40);
+
+		iconCancel = new ImageIcon("src/assets/btnCancel.png");
+		imageCancel = iconCancel.getImage().getScaledInstance(98, 38, Image.SCALE_SMOOTH);
+		iconCancel.setImage(imageCancel);
+
+		btnCancel = new JButton(iconCancel);
+		btnCancel.setBounds(170, 80, 100, 40);
 		btnCancel.setBackground(new Color(38, 38, 38));
 		btnCancel.setFocusPainted(false);
 		btnCancel.setBorderPainted(false);
 		btnCancel.setContentAreaFilled(false);
-		btnCancel.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e) 
-		    {
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				exitFrame.dispose();
-		    }          
+			}
 
 		});
-	    
+		
+		pnlexitHdr.add(btnCloseExit);
+		exitFrame.add(btnOK);
+		exitFrame.add(btnCancel);
 		exitFrame.getContentPane().add(pnlexitHdr);
-	    pnlexitHdr.add(btnCloseExit);
-	    exitFrame.add(btnOK);
-	    exitFrame.add(btnCancel);
-	    exitFrame.repaint();
-	    
-	    MoveMouseListener mml2 = new MoveMouseListener(pnlexitHdr);
-	    pnlexitHdr.addMouseListener(mml2);
-	    pnlexitHdr.addMouseMotionListener(mml2);
+		exitFrame.repaint();
+
+		MoveMouseListener mml2 = new MoveMouseListener(pnlexitHdr);
+		pnlexitHdr.addMouseListener(mml2);
+		pnlexitHdr.addMouseMotionListener(mml2);
 	}
 }
